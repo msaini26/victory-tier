e5d2dff8f19a6a606926985fa582f3f59f5eac2b
Rajan Saini <rajansaini@cs.ucsb.edu>
2020-12-18 18:04:24 -0800

    Add a working dummy backend

--- backend/server.py
+++ backend/server.py
@@ -2,17 +2,25 @@
 Serves a page and responds to requests
 """
 import websockets
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+# Serve the main site (search bar)
 @app.route('/', methods=['GET', 'POST'])
 def form():
-    return render_template('form.html')
+    return render_template('./index.html')
 
-@app.route('/hello', methods=['GET', 'POST'])
+# Retrieve the closest three basketball players
+@app.route('/search', methods=['GET', 'POST'])
 def hello():
-    return render_template('greeting.html', say=request.form['say'], to=request.form['to'])
+    if request.method == 'POST':
+        query = request.form['q']       # q for query
+    else:
+        query = request.args.get('q')   # q for query
+
+    # Reply with the three players
+    return render_template('results.html', p1='LeBron James', p2='Michael Jordan', p3='Rajon Rondo')
 
 if __name__ == "__main__":
     app.run()
