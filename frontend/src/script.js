8505af9abf1a980f3219528c4c33cb450862c36f
Rajan Saini <rajansaini@cs.ucsb.edu>
2020-12-19 21:35:38 -0800

    Add a few method stubs

--- frontend/src/script.js
+++ frontend/src/script.js
@@ -1,8 +1,26 @@
+
+/**
+ * Scrolls down the page
+ */
+function scroll() {
+	// TODO Implement
+}
+
+/**
+ * Performs a search and updates the DOM.
+ * Note: this function is asynchronous
+ */
+function search() {
+	// TODO Implement
+}
+
+
 /**
  * Callback for when the user searches for a player
  */
 function onSearch() {
-	console.log("Search!")
+	search();
+	scroll();
 }
 
 /**
