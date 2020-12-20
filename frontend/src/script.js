507c3d6c2a4682933ca4ebe550b629d6234ae4a9
Mansi Saini <mansisaini26@gmail.com>
2020-12-19 22:26:49 -0800

    click search to scroll down

--- frontend/src/script.js
+++ frontend/src/script.js
@@ -7,7 +7,7 @@ document.getElementById("myDiv").onscroll = function(){scroll()};
 
 function scroll() {
    document.getElementyById("test").innerHTML = "YOU SCROLLED IN DIV.";
-   window.scroll(0,100);
+   window.scroll(0, -5000);
 }
 
 /**
